fb0d4f45f63b55123d0794eb5009d53b451eeb00
tomekg007 <tomaszgubernat7@gmail.com>
2018-11-14 15:05:27 +0100

    test rename

--- ExpressionFuncTests/UnitTest1.cs
+++ ExpressionFuncTests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace ExpressionFuncTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Tests()
         {
 
             Assert.AreEqual(true, true);
