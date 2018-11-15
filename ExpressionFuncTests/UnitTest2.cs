d92d1effd05465aaa90397451f939a977cea485d
tomekg007 <tomaszgubernat7@gmail.com>
2018-11-15 13:48:13 +0100

    test m

---
+++ ExpressionFuncTests/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionFuncTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void Tests()
+        {
+
+            Assert.AreEqual("moshe", "moshe");
+        }
+
+      }
+}
